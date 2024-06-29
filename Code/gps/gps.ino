625c07462a431618f23cd4df46b523e9055f5f12
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-29 13:35:10 -0400

    different code

--- Code/gps/gps.ino
+++ Code/gps/gps.ino
@@ -8,30 +8,24 @@ TinyGPSPlus gps;
 SoftwareSerial ss(GPS_RX_PIN, GPS_TX_PIN);
 
 void setup() {
-  Serial.begin(9600);
-  ss.begin(9600);
+  Serial.begin(115200);
+  Serial.println("GPS Example");
+  ss.begin(4800);
   delay(1000);
 
-  ss.print("$PCAS11,5*18\r\n"); // Set the dynamic model to be airborne with <1g acceleration.
-  Serial.print(ss.read());
-  delay(1000);
+//   ss.print("$PCAS11,5*18\r\n"); // Set the dynamic model to be airborne with <1g acceleration.
+//   Serial.print(ss.read(), HEX);
+//   delay(1000);
 }
 
 void loop() {
-  while (ss.available() > 0) {
-    if (gps.encode(ss.read())) {
-      if (gps.location.isValid()) {
-        double latitude = gps.location.lat();
-        double longitude = gps.location.lng();
-        double altitude = gps.altitude.meters();
-
-        Serial.print("Latitude: ");
-        Serial.println(latitude, 6);
-        Serial.print("Longitude: ");
-        Serial.println(longitude, 6);
-        Serial.print("Altitude: ");
-        Serial.println(altitude, 2);
-      }
-    }
+  if (gps.location.isValid()) {
+    Serial.print(F("Latitude: "));
+    Serial.println(gps.location.lat(), 6);
+    Serial.print(F("Longitude: "));
+    Serial.println(gps.location.lng(), 6);
+  } else {
+    Serial.println(F("Waiting for valid GPS signal..."));
   }
+  delay(1000);
 }
