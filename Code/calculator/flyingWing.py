388f9a5f448740b91a21e5159624cb3bbd1f7475
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-07-01 07:58:46 -0400

    Commit

--- Code/calculator/flyingWing.py
+++ Code/calculator/flyingWing.py
@@ -1,7 +1,11 @@
+import math
+
 # Settings for the flying wing.
 wcl = 7  # Wing cube loading of the glider.
 ar = 6  # Aspect ratio fo the wing.
+taper = 0.45  # Taper ratio of the wing.
 weight = 200  # Weight in grams of the projected glider.
+sweep = 25 # Sweep angle of the wing.
 
 sa = (weight / wcl) ** (1 / 1.5)
 
@@ -11,6 +15,13 @@ span = (ar * saMM) ** (1 / 2)
 
 chord = span / ar
 
+tipChord = chord * (taper*2)
+
+rootChord = chord * (taper/2 + 1)
+
 print("SA:", sa, "dm^2")
 print("Span:", span, "mm")
 print("Chord:", chord, "mm")
+print("Tip chord:", tipChord, "mm")
+print("Root chord:", rootChord, "mm")
+print("Sweep:", sweep, "degrees")
