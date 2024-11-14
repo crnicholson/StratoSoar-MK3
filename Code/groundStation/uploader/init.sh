3f6018e95222a794d07c1db11bd0934ba5b5fad3
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-11-13 21:06:29 -0500

    Commit

--- Code/groundStation/uploader/init.sh
+++ Code/groundStation/uploader/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+source /home/cnicholson/StratoSoar-MK3/Code/groundStation/uploader/env/bin/activate
 python3 -u /home/cnicholson/StratoSoar-MK3/Code/groundStation/uploader/flaskUploader.py
