42df5978f2e271eea353652140a7f12bba319aec
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-12-01 10:43:03 -0500

    Commit

--- Code/groundStation/uploader/init.sh
+++ Code/groundStation/uploader/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-source /home/cnicholson/StratoSoar-MK3/Code/groundStation/uploader/env/bin/activate
-python3 -u /home/cnicholson/StratoSoar-MK3/Code/groundStation/uploader/main.py
+cd /home/cnicholson/StratoSoar-MK3/Code/groundStation/uploader
+source env/bin/activate
+python3 -u main.py
